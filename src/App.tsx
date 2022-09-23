5f9f65ae31a08cfaee927648cc4b84753053315e
Felipe Pizzio <felipetpizzio@gmail.com>
2022-09-22 22:20:46 -0300

    add globalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { GlobalStyle } from "styles/global";
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <GlobalStyle />
+      <div>
+      </div>
+    </>
   );
 }
 
---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    --branco: #FFFFFF;
+    --roxo: #9B00F0;
+    --azul: #263072;
+    --cinza-100: #F2F2F2;
+    --cinza-300: #BBBBBB;
+    --cinza-700: #777777; 
+    --cinza-900: #424242;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat';
+  }
+
+  body {
+    background-color: var(--branco);
+  }
+`;
